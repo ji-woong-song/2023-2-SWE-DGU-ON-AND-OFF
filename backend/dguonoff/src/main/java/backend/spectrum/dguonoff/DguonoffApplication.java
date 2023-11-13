e609e5f0ec338c4eed4936fdac100f6727fedaaf
pletain <pletain@dgu.ac.kr>
2023-11-13 14:09:10 +0900

    Initial backend project

---
+++ backend/dguonoff/src/main/java/backend/spectrum/dguonoff/DguonoffApplication.java
@@ -0,0 +1,13 @@
+package backend.spectrum.dguonoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DguonoffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DguonoffApplication.class, args);
+	}
+
+}
