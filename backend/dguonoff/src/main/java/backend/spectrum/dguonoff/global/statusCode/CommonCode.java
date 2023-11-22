53746ac015b9950d6c3b2d0da5d68adca0535e31
pletain <pletain@dgu.ac.kr>
2023-11-22 14:48:57 +0900

    fix: SUCCESS_EMPOWERMENT 시 출력 메시지 속 %d타입 %s로 변경 (#10)

--- backend/dguonoff/src/main/java/backend/spectrum/dguonoff/global/statusCode/CommonCode.java
+++ backend/dguonoff/src/main/java/backend/spectrum/dguonoff/global/statusCode/CommonCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum CommonCode {
     //User
-    SUCCESS_EMPOWERMENT(HttpStatus.OK, "%d 계정에 관리자 권한이 부여됐습니다."),
+    SUCCESS_EMPOWERMENT(HttpStatus.OK, "%s 계정에 관리자 권한이 부여됐습니다."),
 
     ;
     private final HttpStatus status;
