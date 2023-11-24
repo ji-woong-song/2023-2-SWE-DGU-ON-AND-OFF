1158a275138ba87442579ccb378fec767c74ba05
seonwoonam <potatomoney716@gmail.com>
2023-11-24 18:27:01 +0900

    Feat : 메인페이지 추가

--- frontend/dguonoff/src/App.tsx
+++ frontend/dguonoff/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AdminHome from './components/pages/admin/home/AdminHome';
 import AdminLogin from './components/pages/admin/login/AdminLogin';
 import AdminSignup from './components/pages/admin/signup/AdminSignup';
+import MainPage from './components/pages/service/home/MainPage';
 import useModalCreater from './modules/modal/Modal';
 
 
@@ -16,6 +17,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<MainPage/>} />
           <Route path="/admin" element={<AdminHome />} />
           <Route path="/admin/login" element={<AdminLogin />} />
           <Route path="/admin/signup" element={<AdminSignup />} />
---
+++ frontend/dguonoff/src/components/pages/service/home/MainPage.tsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+
+export default function MainPage() {
+    const appName : String = "동국 ON/OFF";
+
+    return (
+        <div>
+            <h1>{appName}</h1>
+        </div>
+    );
+}
