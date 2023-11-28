5117cc1c497f2f90f7ff754d75afc911b9041a52
MINJU <135093109+minij02@users.noreply.github.com>
2023-11-28 15:21:42 +0900

    Add files via upload

--- frontend/dguonoff/src/modules/reservationButton/ReservationButton.tsx
+++ frontend/dguonoff/src/modules/reservationButton/ReservationButton.tsx
@@ -1,1 +1,21 @@
+// ReserveButton.tsx
+import React from 'react';
+import styles from "./ReservationButton.module.css"; // CSS 모듈 가져오기
 
+interface ReservationButtonProps {
+  onClick?: () => void;
+}
+
+const ReservationButton: React.FC<ReservationButtonProps> = ({ onClick }) => {
+  return (
+    <div className={styles.reservationButtonContainer}>
+      <button
+        className={styles.reservationButton} // CSS 모듈 클래스 적용
+        onClick={onClick}
+      />
+        예약하기
+    </div>
+  );
+};
+
+export default ReservationButton;
