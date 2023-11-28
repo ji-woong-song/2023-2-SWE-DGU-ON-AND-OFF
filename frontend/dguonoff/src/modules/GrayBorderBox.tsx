ee8dcaa1f89919dc22f1b447db682e93d7a02652
seonwoonam <potatomoney716@gmail.com>
2023-11-28 12:44:39 +0900

    Fix : 공용 모듈 수정

--- frontend/dguonoff/src/modules/GrayBorderBox.tsx
+++ frontend/dguonoff/src/modules/GrayBorderBox.tsx
@@ -8,7 +8,7 @@ function GrayBorderBox({ children, style } : { children: ReactNode; style?: SxPr
         square={false} // 'false'로 설정하여 기본적인 테두리 반경을 사용할 수 있게
         sx={{
         my: 2, // 상하 마진
-        p: 2, // 내부 패딩
+        p: 3, // 내부 패딩
         border: '2px solid #D9D9D9',
         borderRadius: '24px', // 테두리 반경 지정
         ...style, // 추가 스타일 적용
--- frontend/dguonoff/src/modules/GrayCircle.tsx
+++ frontend/dguonoff/src/modules/GrayCircle.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react';
 import Box from '@mui/material/Box';
 import { SxProps } from '@mui/material';
 
-function GrayCircle({ radius, imgUrl, imgDescription, style } : { radius: number; imgUrl?: string; imgDescription?: string, style?: SxProps;}) {
+function GrayCircle({ radius, icon, style } : { radius: number; icon : React.ReactElement; style?: SxProps;}) {
     return (
       <Box
         sx={{
@@ -18,11 +18,7 @@ function GrayCircle({ radius, imgUrl, imgDescription, style } : { radius: number
           ...style
         }}
       >
-        <img 
-          src = {imgUrl} // 이미지 주소
-          alt= {imgDescription} // 이미지에 대한 간단한 설명
-          style={{ height: '100%' }} // 이미지의 높이를 조정하여 원 안에 맞게 조절.
-        />
+         {icon} 
       </Box>
     );
   }
