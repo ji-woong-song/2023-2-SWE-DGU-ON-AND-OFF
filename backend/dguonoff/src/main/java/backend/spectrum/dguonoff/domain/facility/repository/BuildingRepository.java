153a0eb8f930dd5bd7f5ebdd38e4cb0a621b280e
shortboy7 <thdwldnd7@naver.com>
2023-11-29 23:38:58 +0900

    fix: server address 제거 2트

--- backend/dguonoff/src/main/java/backend/spectrum/dguonoff/domain/facility/repository/BuildingRepository.java
+++ backend/dguonoff/src/main/java/backend/spectrum/dguonoff/domain/facility/repository/BuildingRepository.java
@@ -5,6 +5,7 @@ import backend.spectrum.dguonoff.DAO.Building;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface BuildingRepository extends JpaRepository<Building, String> {
 }
