57c08f160c1b5ba5f9ec333e077700249837316a
MINJU <135093109+minij02@users.noreply.github.com>
2023-11-30 07:36:00 +0900

    Update App.tsx

--- frontend/dguonoff/src/App.tsx
+++ frontend/dguonoff/src/App.tsx
@@ -5,11 +5,11 @@ import AdminLogin from './components/pages/admin/login/AdminLogin';
 import AdminSignup from './components/pages/admin/signup/AdminSignup';
 import MainPage from './components/pages/service/home/MainPage';
 import useModalCreater from './modules/modal/Modal';
-import ReservationInfo from './components/pages/service/information/ReservationInfo';
+import ReservationInfo from './components/pages/service/reservation/ReservationInfo';
 import LoginPage from './components/pages/service/login/LoginPage';
 import SignupPage from './components/pages/service/signup/SignupPage';
 import UserInfo from './components/pages/service/information/UserInfo';
-
+import ReservationDetailPage from './components/pages/service/reservation/ReservationDetailPage';
 
 function App() {
   // Hook
