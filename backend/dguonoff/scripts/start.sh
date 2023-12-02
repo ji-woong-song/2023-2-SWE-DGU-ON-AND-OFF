5b1b27c4871c8566c91f4d3570da241f91901920
shortboy7 <thdwldnd7@naver.com>
2023-12-03 03:58:46 +0900

    fix: script 수정

--- backend/dguonoff/scripts/start.sh
+++ backend/dguonoff/scripts/start.sh
@@ -14,3 +14,17 @@ source $ENV_FILE
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+
+# jar 파일 실행
+echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
+# .env 파일이 존재하는지 확인
+if [ -f "$ENV_FILE" ]; then
+  # .env 파일이 존재하면 모든 환경 변수를 -D 옵션에 추가
+  nohup java $(cat "$ENV_FILE" | xargs -I {} echo -n "-D{} ") -jar $JAR_FILE  > $APP_LOG 2> $ERROR_LOG &
+else
+  # .env 파일이 없으면 그냥 .jar 파일 실행
+  echo "env파일 없음!\n"
+fi
+
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
--- backend/dguonoff/scripts/stop.sh
+++ backend/dguonoff/scripts/stop.sh
@@ -1,8 +1,3 @@
-if [ -z $CURRENT_PID ]; then
-  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
-else
-  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
-"./scripts/stop.sh" 20L, 530B                                                                                                                                                                                                                                                                         1,1           Top
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/app"
@@ -20,3 +15,6 @@ if [ -z $CURRENT_PID ]; then
   echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
 else
   echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
+  kill -15 $CURRENT_PID
+fi
+
