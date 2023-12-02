08daaf2a3069da787886f9308bc7d8b9d78c07d1
shortboy7 <thdwldnd7@naver.com>
2023-12-03 03:27:16 +0900

    feat:로그인 시 role 정보 알려주는 api 추가, master controller swagger 반영안되는거 수정 시도

--- backend/dguonoff/src/main/java/backend/spectrum/dguonoff/domain/user/api/MasterAdminController.java
+++ backend/dguonoff/src/main/java/backend/spectrum/dguonoff/domain/user/api/MasterAdminController.java
@@ -9,15 +9,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static backend.spectrum.dguonoff.global.statusCode.CommonCode.SUCCESS_EMPOWERMENT;
 
-@Controller
+@RestController
 @Slf4j
 @RequestMapping("/admin/master")
 @RequiredArgsConstructor
@@ -28,11 +28,7 @@ public class MasterAdminController {
     //관리자 권한 부여 기능
     @PostMapping("/empowerment")
     public ResponseEntity<String> grantAdminAuthority(@RequestBody EmpowermentParams empParams){
-//        String adminId = empParams.getAdminId();
-//        String adminId = principal.getName();
         String userId = empParams.getUserId();
-
-//        userService.checkMasterAdmin(adminId);
         User targetUser = userService.findUser(userId);
         userService.changeRoleToAdmin(targetUser);
 
