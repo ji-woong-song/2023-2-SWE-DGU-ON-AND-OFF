e977b5f188126289292e86a49efefc907e1eedb1
Dice <102275981+Dice15@users.noreply.github.com>
2023-12-04 06:10:56 +0900

    Update Building.ts

--- frontend/dguonoff/src/types/Building.ts
+++ frontend/dguonoff/src/types/Building.ts
@@ -1,19 +1,19 @@
 /**
  * Building 클래스는 건물 정보를 나타냅니다.
- * 이 클래스는 건물의 이름을 관리하며, 이름의 조회 및 설정 기능을 제공합니다.
+ * 이 클래스는 건물의 이름과 총 층수를 관리하며, 이름과 총 층수의 조회 및 설정 기능을 제공합니다.
  */
 export default class Building {
     private name: string;
-    private maxfloor: number;
+    private maxFloor: number;
 
     /**
      * Building 클래스의 생성자입니다.
      * @param {string} name - 건물의 이름입니다. 기본값은 빈 문자열("")입니다.
-     * @param {number} maxfloor - 건물의 층수입니다. 기본값은 0입니다.
+     * @param {number} maxFloor - 건물의 총 층수입니다. 기본값은 0입니다.
      */
-    constructor(name: string = "", maxfloor: number = 0) {
+    constructor(name: string = "", maxFloor: number = 0) {
         this.name = name;
-        this.maxfloor = maxfloor;
+        this.maxFloor = maxFloor;
     }
 
     // 이름 관련 getter 및 setter
@@ -25,12 +25,12 @@ export default class Building {
         this.name = name;
     }
 
-    // 층 정보에 대한 getter 및 setter
-  public getMaxloor(): number {
-    return this.maxfloor;
-  }
+    // 층수 관련 getter 및 setter
+    public getMaxFloor(): number {
+        return this.maxFloor;
+    }
 
-  public setMaxloor(maxfloor: number): void {
-    this.maxfloor = maxfloor;
-  }
+    public setMaxFloor(floor: number): void {
+        this.maxFloor = floor;
+    }
 };
