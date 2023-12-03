af617282a26e37a17faca55bc3857858a2a4eb2c
MINJU <135093109+minij02@users.noreply.github.com>
2023-12-03 21:42:27 +0900

    Update Building.ts

--- frontend/dguonoff/src/types/Building.ts
+++ frontend/dguonoff/src/types/Building.ts
@@ -4,13 +4,16 @@
  */
 export default class Building {
     private name: string;
+    private maxfloor: number;
 
     /**
      * Building 클래스의 생성자입니다.
      * @param {string} name - 건물의 이름입니다. 기본값은 빈 문자열("")입니다.
+     * @param {number} maxfloor - 건물의 층수입니다. 기본값은 0입니다.
      */
-    constructor(name: string = "") {
+    constructor(name: string = "", maxfloor: number = 0) {
         this.name = name;
+        this.maxfloor = maxfloor;
     }
 
     // 이름 관련 getter 및 setter
@@ -21,4 +24,13 @@ export default class Building {
     public setName(name: string): void {
         this.name = name;
     }
+
+    // 층 정보에 대한 getter 및 setter
+  public getMaxloor(): number {
+    return this.maxfloor;
+  }
+
+  public setMaxloor(maxfloor: number): void {
+    this.maxfloor = maxfloor;
+  }
 };
