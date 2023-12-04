c888b19d6befde30ac502deb9582f6951eac38c9
shortboy7 <thdwldnd7@naver.com>
2023-12-05 01:19:19 +0900

    fix:cors 범위, 고정시간표 조회 범위 수정

--- backend/dguonoff/src/main/java/backend/spectrum/dguonoff/security/config/SecurityConfig.java
+++ backend/dguonoff/src/main/java/backend/spectrum/dguonoff/security/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5500"));
-        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowedMethods(List.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "TRACE"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
