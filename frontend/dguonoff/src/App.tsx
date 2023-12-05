020aba7291cd3edcc707fdac9904e3dfb040216e
seonwoonam <potatomoney716@gmail.com>
2023-12-05 22:26:34 +0900

    Fix : 예약정보 페이지 수정

--- frontend/dguonoff/src/App.tsx
+++ frontend/dguonoff/src/App.tsx
@@ -8,10 +8,22 @@ import useModalCreater from './modules/modal/Modal';
 import ReservationInfo from './components/pages/service/information/ReservationInfo';
 import LoginPage from './components/pages/service/login/LoginPage';
 import SignupPage from './components/pages/service/signup/SignupPage';
+import React, { createContext, useState } from 'react';
+import Reservation from './types/Reservation';
 // import UserInfo from './components/pages/service/information/UserInfo';
 // import ReservationDetailPage from './components/pages/service/reservation/ReservationDetailPage';
 
+export const ReservationContext = React.createContext({
+  reservationInfo : new Reservation(), // 기본값
+  setReservationInfo: (resrvation : Reservation) => {}, // 빈 함수로 초기화
+});
+
 function App() {
+  const [reservationInfo, setReservation] = useState<Reservation>(new Reservation());
+
+  const setReservationInfo = (reservation : Reservation): void => {
+    setReservation(reservation);
+  };
   // Hook
   useModalCreater();
 
@@ -19,21 +31,24 @@ function App() {
   // Render
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage/>} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/reservationInfo" element={<ReservationInfo/>} />
-          {/* <Route path="/userInfo" element={<UserInfo/>} />
-          <Route path="/reservationPage" element={<ReservationPage />} />
-          <Route path="/onoffPage" element={<OnOffPage />} />
-          <Route path="/reservationdetailPage" element={<ReservationDetailPage />}/> */}
-          <Route path="/admin" element={<AdminHome />} />
-          <Route path="/admin/login" element={<AdminLogin />} />
-          <Route path="/admin/signup" element={<AdminSignup />} />
-        </Routes>
-      </BrowserRouter>
+      <ReservationContext.Provider value={{reservationInfo, setReservationInfo}}>
+        <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<MainPage/>} />
+              <Route path="/reservationInfo" element={<ReservationInfo reservation={reservationInfo!}/>} />
+
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            {/* <Route path="/userInfo" element={<UserInfo/>} />
+            <Route path="/reservationPage" element={<ReservationPage />} />
+            <Route path="/onoffPage" element={<OnOffPage />} />
+            <Route path="/reservationdetailPage" element={<ReservationDetailPage />}/> */}
+            <Route path="/admin" element={<AdminHome />} />
+            <Route path="/admin/login" element={<AdminLogin />} />
+            <Route path="/admin/signup" element={<AdminSignup />} />
+          </Routes>
+        </BrowserRouter>
+      </ReservationContext.Provider>
     </div>
   );
 }
